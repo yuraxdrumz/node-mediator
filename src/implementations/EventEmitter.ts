3bdd04e95ef06d82d3d8d41779dbf8d703aba8f9
yura <yurik1776@gmail.com>
2018-12-28 18:52:38 +0200

    added initial tests and prettierignore

--- src/implementations/EventEmitter.ts
+++ src/implementations/EventEmitter.ts
@@ -4,15 +4,14 @@ import { Listener } from '../types/Listener'
 
 // an implemenatation of the emitter interface with extension of a wildcard supported event emitter
 export class EventEmitter extends EventEmitter2 implements Emitter {
-    constructor(options) {
-      super(options)
-    }
-    on(event: string | string[], listener: Listener) {
-      return super.on(event, listener)
-    }
-  
-    emit(event: string | string[], ...values: any[]) {
-      return super.emit(event, ...values)
-    }
+  constructor(options) {
+    super(options)
   }
-  
+  on(event: string | string[], listener: Listener) {
+    return super.on(event, listener)
+  }
+
+  emit(event: string | string[], ...values: any[]) {
+    return super.emit(event, ...values)
+  }
+}
