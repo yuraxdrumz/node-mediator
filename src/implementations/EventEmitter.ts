79e821c4c28436d1142344e7f5babf4dc6b357a7
yura <yurik1776@gmail.com>
2018-12-28 19:02:57 +0200

    removed unneccessary methods that are already implemented on event emitter

--- src/implementations/EventEmitter.ts
+++ src/implementations/EventEmitter.ts
@@ -1,17 +1,5 @@
 import { EventEmitter2 } from 'eventemitter2'
 import { Emitter } from '../interfaces/Emitter'
-import { Listener } from '../types/Listener'
 
 // an implemenatation of the emitter interface with extension of a wildcard supported event emitter
-export class EventEmitter extends EventEmitter2 implements Emitter {
-  constructor(options) {
-    super(options)
-  }
-  on(event: string | string[], listener: Listener) {
-    return super.on(event, listener)
-  }
-
-  emit(event: string | string[], ...values: any[]) {
-    return super.emit(event, ...values)
-  }
-}
+export class EventEmitter extends EventEmitter2 implements Emitter {}
