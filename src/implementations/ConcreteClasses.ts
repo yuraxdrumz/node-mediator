8117b6a8072a79699d39ba7f5ddb1295b51a8fda
yura <yurik1776@gmail.com>
2018-12-29 16:23:02 +0200

    removed unneccesasry imports from classes

--- src/implementations/ConcreteClasses.ts
+++ src/implementations/ConcreteClasses.ts
@@ -1,6 +1,4 @@
-import { Mediator } from '../interfaces/Mediator'
 import { Colleague } from '../abstracts/Colleague'
-import { ColleagueName } from '../types/ColleagueName'
 
 // here are a few example classes that extend the Colleague abstract class to inherit on and emit.
 class Logger extends Colleague {
