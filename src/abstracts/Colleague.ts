59ad0e26bb3f3083562f7b6ea8889da55364f250
yura <yurik1776@gmail.com>
2019-01-03 21:37:24 +0200

    added tsdoc

--- src/abstracts/Colleague.ts
+++ src/abstracts/Colleague.ts
@@ -2,7 +2,6 @@ import * as Types from '../types/Listener'
 import Mediator from '../interfaces/Mediator'
 import { ColleagueName } from '../types/Colleague'
 
-// a colleague is an abstract class that you inherit from with on and emit in place
 export default abstract class Colleague {
   private mediator: Mediator
   public readonly name: ColleagueName
@@ -13,12 +12,10 @@ export default abstract class Colleague {
   emit(event: string, ...args: any[]): void {
     this.mediator.emit(this, event, ...args)
   }
-
   emitAsync(event: string, ...args: any[]): Promise<any> {
     return this.mediator.emitAsync(this, event, ...args)
   }
-
-  on(event: string, cb: Types.Listener): void {
+  on(event: string, cb: Types.Listener): Promise<any> | void {
     this.mediator.on(this, event, cb)
   }
   register() {
